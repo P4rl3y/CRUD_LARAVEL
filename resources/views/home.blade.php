1ad119ad2e1d432e222ff050071705e767a294e4
P4rl3y <hola.juan.9.32@gmail.com>
2021-01-03 20:25:55 -0600

    Vista y controlador de home (esqueleto)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('title', 'Home')
+    
+@section('content')
+    <div class="container">
+        <h1 class="font-sans text-3xl font-bold text-center">CRUD_LARAVEL</h1>
+        <p class="font-sans text-md text-center">Practica: Un crud elaborado en en framework laravel de php, para poner enpractica conocimientos basicos sobre el mismo.</p>
+    </div>
+@endsection
